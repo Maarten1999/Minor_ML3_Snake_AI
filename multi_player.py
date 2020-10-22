0a6b5dc9ba577b6cae75a7f3abe157bc23a8ff35
Ruben <rwoldhui@avans.nl>
2020-10-22 22:58:28 +0200

    [Added] basic DQN from stable_baselines to single_player snake

--- multi_player.py
+++ multi_player.py
@@ -2,6 +2,7 @@ import logging
 
 # import gym_snake  # don't use the registered snake
 from gym_snake.envs.snake_env import SnakeEnv
+from path_finding import path_finder
 
 log = logging.getLogger("miniproject_snake")
 log.setLevel(logging.INFO)
@@ -13,16 +14,50 @@ RIGHT = 1
 DOWN = 2
 LEFT = 3
 
+n_snakes = 2
+snakes = []
+for i in range(n_snakes):
+    snakes.append(path_finder.BFS())
+
 # env = SnakeEnv(grid_size=[12, 12], snake_size=2, n_snakes=3, n_foods=3)
-env = SnakeEnv(grid_size=[9, 9], snake_size=2, n_snakes=2, n_foods=2)
+env = SnakeEnv(grid_size=[9, 9], snake_size=2, n_snakes=n_snakes, n_foods=2)
 obs = env.reset()  # construct instance of game
-done = False
+done = n_snakes
+turn = 0
 log.info("start game")
-for i in range(30):
-    if not done:
+while done > 0:
+    env.render()
+
+    actions_1 = snakes[0].get_actions(obs, 9, 9)
+    actions_2 = snakes[1].get_actions(obs, 9, 9)
+
+    if len(actions_1) == 0:
+        done -= 1
+    if len(actions_2) == 0:
+        done -= 1
+
+    total = len(actions_1) + len(actions_2)
+    counter_1 = 0
+    counter_2 = 0
+    for i in range(total):
+        action = -1
+        if turn == 0:
+            action = actions_1[counter_1]
+            counter_1 += 1
+        if turn == 0:
+            action = actions_2[counter_2]
+            counter_2 += 1
+        turn += 1
+        if turn >= n_snakes:
+            turn = 0
+
+        obs, reward, _, info = env.step(action)  # pass action to step()
         env.render()
-        # action = [DOWN, DOWN]  # *normal* multi-player snake: all snakes move at the same time
-        action = DOWN  # turn-based multi-player snake: one snake moves, other snakes don't move
-        obs, reward, done, info = env.step(action)  # reward is for the snake that has moved
-        log.info("reward: %.0f", reward)
+
+    print(done)
+
+    # # action = [DOWN, DOWN]  # *normal* multi-player snake: all snakes move at the same time
+    # action = DOWN  # turn-based multi-player snake: one snake moves, other snakes don't move
+    # obs, reward, done, info = env.step(action)  # reward is for the snake that has moved
+    # log.info("reward: %.0f", reward)
 env.close()
---
+++ single_player_rl.py
@@ -0,0 +1,47 @@
+import logging
+
+from stable_baselines import DQN
+from stable_baselines.deepq.policies import MlpPolicy
+
+# import gym_snake  # don't use the registered snake
+from gym_snake.envs.snake_env import SnakeEnv
+
+log = logging.getLogger("miniproject_snake")
+log.setLevel(logging.INFO)
+log.addHandler(logging.StreamHandler())
+
+# actions
+UP = 0
+RIGHT = 1
+DOWN = 2
+LEFT = 3
+
+env = SnakeEnv(grid_size=[24, 24], snake_size=2)
+obs = env.reset()  # construct instance of game
+contr = env.controller
+
+training = True
+if training:
+    model = DQN(MlpPolicy, env, verbose=1)
+    model.learn(total_timesteps=20000)
+    model.save("./models/dqn_snake_single_player")
+else:
+    model = DQN.load("./models/dqn_snake_single_player")
+
+print("finished training, now use the trained model and render the env")
+n_episodes = 10
+
+for i in range(n_episodes):
+    obs = env.reset()
+    done = False
+    episode_steps = 0
+    while not done:
+        action, state = model.predict(obs)
+        obs, reward, done, info = env.step(action)
+        episode_steps += 1
+        env.render()
+
+    result = 'Succes!' if episode_steps < 200 else "Failure!"
+    print(result)
+
+env.close()
